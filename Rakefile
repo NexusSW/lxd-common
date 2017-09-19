c31d89056a2db6055d18cbd48e27969505d92c58
Sean Zachariasen <thewyzard@hotmail.com>
2017-09-18 19:51:55 -0600

    added mock transport for a quick smoke test

--- Rakefile
+++ Rakefile
@@ -2,5 +2,8 @@ require 'bundler/gem_tasks'
 require 'rspec/core/rake_task'
 
 RSpec::Core::RakeTask.new(:spec)
+RSpec::Core::RakeTask.new(:mock) do |task|
+  task.pattern = 'spec/**{,/*/**}/*_mock.rb'
+end
 
 task default: :spec
---
+++ spec/lxd/common_mock.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'mock_transport'
+
+describe NexusSW::LXD::Driver do
+  context 'Local CLI Interface' do
+    let(:test_name) { 'lxd-cli-driver-test' }
+    let(:nx_driver) { NexusSW::LXD::Driver::CLI.new ::NexusSW::LXD::Transport::Mock.new }
+    include_examples 'Container Startup'
+    let(:transport) { NexusSW::LXD::Transport::CLI.new nx_driver, NexusSW::LXD::Transport::Mock.new, test_name }
+    include_examples 'Transport Functions'
+    include_examples 'Container Shutdown'
+  end
+end
