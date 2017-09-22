8a4f1ea3da70dc7b576b975c1d1909b19dfdfe44
Sean Zachariasen <thewyzard@hotmail.com>
2017-09-22 16:46:09 -0600

    enable simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'simplecov'
+SimpleCov.start
+
 require 'nexussw/lxd/driver/cli'
 require 'nexussw/lxd/driver/rest'
 require 'nexussw/lxd/transport/cli'
