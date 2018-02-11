8f66e358dec813f00c8a96cdb6a809c80823cd24
Sean Zachariasen <thewyzard@hotmail.com>
2018-02-10 18:38:26 -0700

    improve error handling

--- lib/nexussw/lxd/transport/mixins/helpers/users.rb
+++ lib/nexussw/lxd/transport/mixins/helpers/users.rb
@@ -7,11 +7,13 @@ module NexusSW
         module Helpers
           module UsersMixin
             def user(user_nameorid, options = {})
+              return unless user_nameorid
               passwd = read_file options[:passwd_file] || '/etc/passwd'
 
               # rework into .split(':') if this gets more complicated
               @uid = user_nameorid.is_a?(String) ? passwd[/^#{user_nameorid}:[^:]*:([^:]*):/, 1] : user_nameorid
               @username = user_nameorid.is_a?(String) ? user_nameorid : passwd[/^([^:]*):[^:]*:#{user_nameorid}:/, 1]
+              raise "User not found (#{user_nameorid}) while attempting to set transport identity" unless @uid && @username
 
               # gotcha: we're always setting the default group here, but it's changeable by the user, afterwards
               # so if `user` gets called again, and the caller wants an alternative gid, the caller will need to re-set the gid
